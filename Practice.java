42d9f787872251e31e1aee7ad8e3620d4eea5a74
admin <admin@DESKTOP-UBIHQL7>
2023-11-20 20:08:05 +0530

    practice test2

---
+++ Practice.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Practice {
+	public void mail(string[] arg)
+	{
+	Scanner sc =new scanner(System.in);
+	System.out.println("enter month name");
+	string month=sc.nextline();
+	
+	switch(month no)
+	{
+	case jan :System.out.println("1"); break;
+	case feb:System.out.println("2"); break;
+	case march:System.out.println("3"); break;
+	case aprial:System.out.println("4"); break;
+	case may:System.out.println("5"); break;
+	case june:System.out.println("6"); break;
+	case julay:System.out.println("7"); break;
+	case auguest:System.out.println("8"); break;
+	case september:System.out.println("9"); break;
+	case october:System.out.println("10"); break;	
+	case november:1System.out.println("11"); break;
+	case december:System.out.println("12"); break;
+	default:System.out.println("invalid month no");
+	}
+
+sc.close();
+}
+}
